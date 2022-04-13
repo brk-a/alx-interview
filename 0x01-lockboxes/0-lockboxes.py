a22af490bd0af6c9a30f43230b2d4de0dd8ac922
brk-a <fnjakai@outlook.com>
2022-04-13 04:57:28 -0700

    lockboxes: try v 1.4

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,7 +11,45 @@ Write a method that determines if all the boxes can be opened.
     boxes is a list of lists
     A key with the same number as a box opens that box
     You can assume all keys will be positive integers
-        There can be keys that do not have boxes
+    There can be keys that do not have boxes
     The first box boxes[0] is unlocked
     Return True if all boxes can be opened, else return False
 '''
+
+
+def join(list_1, list_2):
+    '''return a list of elements '''
+    result = []
+    for elem in list_2:
+        result += list_1[elem]
+    return result
+
+
+def canUnlockAll(boxes):
+    ''' return T if all boxes can be opened else, F '''
+    boxes = boxes if boxes and isinstance(boxes, list) else []
+    idx = 0
+    total = list(set(boxes[0]) | {0})
+    can_open = True
+    while can_open:
+        can_open = False
+        for i in join(boxes, total[idx:]):
+            if i not in total:
+                total.append(i)
+                idx += 1
+                can_open = True
+    return len(total) == len(boxes)
+
+
+if __name__ == '__main__':
+    canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
+
