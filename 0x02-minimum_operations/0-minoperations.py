1bf71765b02b1e1d9b3e242ee3d02a6349e1e1ee
brk-a <fnjakai@outlook.com>
2022-04-19 00:00:59 -0700

    min ops v 0.0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+In a text file, there is a single character H.
+Your text editor can execute only two operations in this file:
+Copy All and Paste. Given a number n, write a method that
+calculates the fewest number of operations needed to result
+in exactly n H characters in the file
+'''
+
+
+def minOperations(n):
+    ''' return the min no. of ops '''
+
+
+
+if __name__ == '__main__':
+    minOperations = __import__('0-minoperations').minOperations
+
+    n = 4
+    print("Min # of operations to reach {} char: {}".format\
+          (n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format\
+          (n, minOperations(n)))
