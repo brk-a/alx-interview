c45225121044f0b52367ec6f4f6e39650eebc257
brk-a <fnjakai@outlook.com>
2022-04-21 01:11:53 -0700

    min ops: try v 3.0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,6 +11,14 @@ in exactly n H characters in the file
 
 def minOperations(n):
     ''' return the min no. of ops '''
+    num_ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            num_ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return num_ops
 
 
 
