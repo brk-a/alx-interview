355cddc920b2df5d67bc9b7e9bceaffcde80565b
brk-a <fnjakai@outlook.com>
2022-05-04 23:04:46 -0700

    0-validate_utf8: try v 3.1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,9 +16,27 @@ of each integer
 '''
 
 
-def validUTF(data):
+def validUTF8(data):
     """UTF-8 validator"""
-    pass
+    data = iter(data)
+    for leading_byte in data:
+        leading_ones = _count_leading_ones(leading_byte)
+        if leading_ones in [1, 7, 8]:
+            return False
+        for _ in range(leading_ones - 1):
+            trailing_byte = next(data, None)
+            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+                return False
+    return True
+
+
+def _count_leading_ones(byte):
+    """Counts the leading ones."""
+
+    for i in range(8):
+        if byte >> 7 - i == 0b11111111 >> 7 - i & ~1:
+            return i
+    return 8
 
 
 if __name__ == '__main__':
