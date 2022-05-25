d9aa07a1b00d5378b6e5c1b0454111ce1aca0373
brk-a <fnjakai@outlook.com>
2022-05-25 00:09:01 -0700

    0x07-rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,6 +10,15 @@ You can assume the matrix will have 2 dimensions and will not be empty.
 
 def rotate_2d_matrix(matrix):
     """rotate a 2d matrix in-place"""
+    rotated = []
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)-1, -1, -1):
+            rotated.append(matrix[j][i])
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            matrix[i][j] = rotated.pop(0)
 
 
 if __name__ == '__main__':
@@ -18,6 +27,6 @@ if __name__ == '__main__':
               [4, 5, 6],
               [7, 8, 9]]
 
-    print('Input matrix:\n {}').format(matrix)
+    print('Input matrix:\n {}'.format(matrix))
     rotate_2d_matrix(matrix)
-    print('Output matrix:\n {}').format(matrix)
+    print('Output matrix:\n {}'.format(matrix))
