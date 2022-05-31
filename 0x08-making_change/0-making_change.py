21287a41c025ef83aec9ed8de6c5999622434fd3
brk-a <fnjakai@outlook.com>
2022-05-31 07:00:17 -0700

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,7 +18,21 @@ Your solution’s runtime will be evaluated in this task
 
 
 def makeChange(coins, total):
-    pass
+    """change; change everywhere"""
+    if total < 1:
+        return 0
+    coins.sort()
+    coins.reverse()
+    fewest = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
 
 
 if __name__ == '__main__':
